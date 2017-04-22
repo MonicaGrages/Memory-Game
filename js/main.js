e41b8689788681f7fde16be95fdf600940932a34
Monica Grages <monicagrages@monicas-mbp.attlocal.net>
2017-04-22 14:41:03 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -31,11 +31,13 @@ var checkForMatch = function() {
 			document.querySelector(".matchMessage").style.color="#00A6B3";
 			score++;
 			document.querySelector(".scoreMessage").innerHTML="Your score is: "+score;
+			console.log("User found a match. Score +1")
 
 		;}
 		else {
 			document.querySelector(".matchMessage").innerHTML="Sorry, try again.";
 			document.querySelector(".matchMessage").style.color="#F15B31";
+			console.log("Mismatch")
 		;}
 	};
 	if (cardsInPlay.length ===3) {
@@ -48,25 +50,33 @@ var checkForMatch = function() {
 };
 
 var flipCard = function() {
-	var cardId = this.getAttribute("data-id");
-	this.setAttribute("src", cards[cardId].cardImage);
-	console.log("User flipped "+cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	if (this.getAttribute("src") !== "images/back.png") /*user clicks face up card*/{
+		for (var i=0; i<cards.length; i++) {
+			document.querySelectorAll("img")[i].setAttribute("src", "images/back.png"); /*turn all cards back over*/
+		};	
+		document.querySelector(".matchMessage").innerHTML=" ";
+		cardsInPlay = [];
+	}
+	else {
+		var cardId = this.getAttribute("data-id");
+		this.setAttribute("src", cards[cardId].cardImage);
+		console.log("User flipped "+cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
+		checkForMatch();
+	// console.log(cards[cardId].cardImage);
+	// console.log(cards[cardId].suit);
+	};
 };
 
 var createBoard = function () {
- for (var i = 0; i<cards.length; i++) {
- 	var cardElement = document.createElement("img");
- 	cardElement.setAttribute("src", "images/back.png");
- 	cardElement.setAttribute("data-id", i);
- 	cardElement.addEventListener("click", flipCard);
- 	document.getElementById('game-board').appendChild(cardElement);
- 	cards.sort(function(a,b){return 0.5 - Math.random()});
-
- };
+ 	for (var i = 0; i<cards.length; i++) {
+ 		var cardElement = document.createElement("img");
+	 	cardElement.setAttribute("src", "images/back.png");
+	 	cardElement.setAttribute("data-id", i);
+	 	cardElement.addEventListener("click", flipCard);
+	 	document.getElementById('game-board').appendChild(cardElement);
+ 	};
+ 	cards.sort(function(a,b){return 0.5 - Math.random()}); /*shuffle cards*/
 };
 
 createBoard();
@@ -77,7 +87,10 @@ var cardReset = function() {
 	};
 	cardsInPlay = [];
 	document.querySelector(".matchMessage").innerHTML=" ";
-	cards.sort(function(a,b){return 0.5 - Math.random()});
+	cards.sort(function(a,b) {
+		return 0.5 - Math.random(); /*randomly sorts cards array*/
+	});
+	console.log("Cards were shuffled");
 };
 
 var cardButton = document.getElementById("card-reset");
@@ -86,12 +99,14 @@ cardButton.addEventListener("click", cardReset);
 var score=0;
 document.querySelector(".scoreMessage").innerHTML="Your score is: "+score;
 
-
 var scoreReset = function () {
 	score = 0;
 	document.querySelector(".scoreMessage").innerHTML="Your score is: "+score;
+	console.log("Score was reset");
 
 };
 
 var scoreButton = document.getElementById("score-reset");
 scoreButton.addEventListener("click", scoreReset);
+
+
